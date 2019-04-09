eb0c00eaa2601adedeb74d5955e5ccb08fd2cad6
Sarang Noether <32460187+SarangNoether@users.noreply.github.com>
2019-04-09 13:13:35 -0400

    Schnorr proof uses a proof class

--- lelantus/schnorr.py
+++ lelantus/schnorr.py
@@ -1,9 +1,18 @@
 # Generalized Schnorr proof
 #
-# Shows that the prover knows s,t such that y = s*G + t*H for public y,G,H
+# Shows that the prover knows s,t such that Y = s*G + t*H for public Y,G,H
 
 import dumb25519
 from dumb25519 import Scalar, Point, random_scalar, hash_to_scalar, hash_to_point
+
+# Schnorr proof
+class Proof:
+    G = None
+    H = None
+    Y = None
+    U = None
+    s1 = None
+    t1 = None
 
 # Perform a generalized Schnorr proof
 def prove(s,t,G,H):
@@ -24,21 +33,27 @@ def prove(s,t,G,H):
     s1 = s0 - x*s
     t1 = t0 - x*t
 
-    return [G,H,Y,U,s1,t1]
+    proof = Proof()
+    proof.G = G
+    proof.H = H
+    proof.Y = Y
+    proof.U = U
+    proof.s1 = s1
+    proof.t1 = t1
+    return proof
 
 # Verify a generalized Schnorr proof
 def verify(proof):
-    G,H,Y,U,s1,t1 = proof
     cache = '' # Fiat-Shamir rolling transcript hash
 
     # Update transcript
-    cache = hash_to_scalar(cache,G)
-    cache = hash_to_scalar(cache,H)
-    cache = hash_to_scalar(cache,Y)
-    cache = hash_to_scalar(cache,U)
+    cache = hash_to_scalar(cache,proof.G)
+    cache = hash_to_scalar(cache,proof.H)
+    cache = hash_to_scalar(cache,proof.Y)
+    cache = hash_to_scalar(cache,proof.U)
 
     x = cache # challenge
 
-    if not U == Y*x + s1*G + t1*H:
+    if not proof.U == proof.Y*x + proof.s1*proof.G + proof.t1*proof.H:
         raise ArithmeticError('Invalid proof!')
     return True
