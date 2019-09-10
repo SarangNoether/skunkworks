bf0c793c739a43b8e275ec4a549f0e20cfa4fbd3
Sarang Noether <32460187+SarangNoether@users.noreply.github.com>
2019-09-10 13:07:38 -0400

    Proving system and basic test

--- lrs/groth.py
+++ lrs/groth.py
@@ -1,76 +1,52 @@
-from common import *
-from dumb25519 import hash_to_point, random_scalar, Scalar, hash_to_scalar
+# Proof-of-concept implementation of https://github.com/monero-project/research-lab/issues/56
 
-# Internal proof state
-class State:
-    def __init__(self):
-        self.m = None
-        self.n = None
-        self.sigma = None
-        self.a = None
-        self.rA = None
-        self.rB = None
-        self.rC = None
-        self.rD = None
-        self.v = None
-        self.r = None
-        self.rho = None
-        self.tau = None
-        self.gammas = None
+from dumb25519 import hash_to_point, random_scalar, Scalar, hash_to_scalar, G, random_point
+import dumb25519
 
-        self.x = None # Fiat-Shamir challenge
+H = hash_to_point('H')
 
 # Proof structure
 class Proof:
     def __init__(self):
+        self.J = None
         self.A = None
         self.B = None
         self.C = None
         self.D = None
-        self.G = None
-        self.Q = None
+        self.X = None
+        self.Y = None
+        self.Z = None
         self.f = None
         self.zA = None
         self.zC = None
-        self.zV = None
         self.zR = None
+        self.zS = None
 
     def __repr__(self):
         temp = '<GrothProof> '
+        temp += 'J:'+repr(self.J)+'|'
         temp += 'A:'+repr(self.A)+'|'
         temp += 'B:'+repr(self.B)+'|'
         temp += 'C:'+repr(self.C)+'|'
         temp += 'D:'+repr(self.D)+'|'
-        temp += 'G:'+repr(self.G)+'|'
-        temp += 'Q:'+repr(self.Q)+'|'
+        temp += 'X:'+repr(self.G)+'|'
+        temp += 'Y:'+repr(self.Q)+'|'
+        temp += 'Z:'+repr(self.Z)+'|'
         temp += 'f:'+repr(self.f)+'|'
         temp += 'zA:'+repr(self.zA)+'|'
         temp += 'zC:'+repr(self.zC)+'|'
-        temp += 'zV:'+repr(self.zV)+'|'
-        temp += 'zR:'+repr(self.zR)
+        temp += 'zR:'+repr(self.zR)+'|'
+        temp += 'zS:'+repr(self.zS)
         return temp
 
-# Compute an aggregate Fiat-Shamir challenge
-def challenge(proofs):
-    A = [proof.A for proof in proofs]
-    B = [proof.B for proof in proofs]
-    C = [proof.C for proof in proofs]
-    D = [proof.D for proof in proofs]
-    G = [proof.G for proof in proofs]
-    Q = [proof.Q for proof in proofs]
-    return hash_to_scalar(A,B,C,D,G,Q)
-
-# Double-blinded Pedersen matrix commitment
+# Pedersen vector commitment
 def com_matrix(v,r):
     C = dumb25519.Z
-    for j in range(len(v)):
-        for i in range(len(v[0])):
-            C += hash_to_point('Gi',j,i)*v[j][i]
+    for i in range(len(v)):
+        for j in range(len(v[0])):
+            C += hash_to_point('Gi',i,j)*v[i][j]
+    C += r*H
     return C
-
-# Double-blinded Pedersen commitment
-def comm(s,v,r):
-    return G*s + H1*v + H2*r
 
 # Decompose a value with given base and size
 def decompose(val,base,size,t=int):
@@ -122,26 +98,33 @@ def convolve(x,y,size=None):
 
     return r
 
-# Perform a commitment-to-zero proof
+# Perform a double-base commitment-to-zero proof
 #
 # INPUT
-#  M: list of double-blinded Pedersen commitments such that len(M) == n**m
-#  l: index such that M[l] is a commitment to zero
-#  v: first Pedersen blinder for M[l]
-#  r: second Pedersen blinder for M[l]
-#  n,m: dimensions such that len(M) == n**m
+#  M: public key list
+#  P: value commitment list
+#  l: index such that M[l], C[l] are commitments to zero
+#  r: Pedersen blinder for M[l]
+#  s: Pedersen blinder for C[l]
+#  m: dimension such that len(M) == len(C) == 2**m
 # RETURNS
 #  proof structure
-#  internal state
-def prove_initial(M,l,v,r,n,m):
+def prove(M,P,l,r,s,m):
+    n = 2 # binary decomposition
+
     # Size check
-    if not len(M) == n**m:
+    if not len(M) == n**m or not len(P) == n**m:
         raise IndexError('Bad size decomposition!')
     N = len(M)
 
-    # Reconstruct the known commitment
-    if not comm(Scalar(0),v,r) == M[l]:
-        raise ValueError('Bad known commitment!')
+    # Construct the tag
+    J = r.invert()*hash_to_point(M[l])
+
+    # Reconstruct the known commitments
+    if not M[l] == r*G:
+        raise ValueError('Bad known public key!')
+    if not P[l] == s*G:
+        raise ValueError('Bad known value commitment!')
 
     rA = random_scalar()
     rB = random_scalar()
@@ -149,7 +132,7 @@ def prove_initial(M,l,v,r,n,m):
     rD = random_scalar()
 
     # Commit to zero-sum blinders
-    a = [[random_scalar()]*n for _ in range(m)]
+    a = [[random_scalar() for _ in range(n)] for _ in range(m)]
     for j in range(m):
         a[j][0] = Scalar(0)
         for i in range(1,n):
@@ -158,28 +141,28 @@ def prove_initial(M,l,v,r,n,m):
 
     # Commit to decomposition bits
     decomp_l = decompose(l,n,m)
-    sigma = [[None]*n for _ in range(m)]
+    sigma = [[None for _ in range(n)] for _ in range(m)]
     for j in range(m):
         for i in range(n):
             sigma[j][i] = delta(decomp_l[j],i)
     B = com_matrix(sigma,rB)
 
     # Commit to a/sigma relationships
-    a_sigma = [[Scalar(0)]*n for _ in range(m)]
+    a_sigma = [[Scalar(0) for _ in range(n)] for _ in range(m)]
     for j in range(m):
         for i in range(n):
             a_sigma[j][i] = a[j][i]*(Scalar(1) - Scalar(2)*sigma[j][i])
     C = com_matrix(a_sigma,rC)
     
     # Commit to squared a-values
-    a_sq = [[Scalar(0)]*n for _ in range(m)]
+    a_sq = [[Scalar(0) for _ in range(n)] for _ in range(m)]
     for j in range(m):
         for i in range(n):
             a_sq[j][i] = -a[j][i]*a[j][i]
     D = com_matrix(a_sq,rD)
 
     # Compute p coefficients
-    p = [[Scalar(0)]*m for _ in range(N)]
+    p = [[Scalar(0) for _ in range(m)] for _ in range(N)]
     for k in range(N):
         decomp_k = decompose(k,n,m)
         p[k] = [a[0][decomp_k[0]],delta(decomp_l[0],decomp_k[0])]
@@ -188,112 +171,83 @@ def prove_initial(M,l,v,r,n,m):
             p[k] = convolve(p[k],[a[j][decomp_k[j]],delta(decomp_l[j],decomp_k[j])],m)
 
     # Generate proof values
-    G = [dumb25519.Z]*m
-    Q = [dumb25519.Z]*m
-    rho = [None]*m
-    tau = [None]*m
-    gammas = [None]*m
+    X = [dumb25519.Z for _ in range(m)]
+    Y = [dumb25519.Z for _ in range(m)]
+    Z = [dumb25519.Z for _ in range(m)]
+    rho = [random_scalar() for _ in range(m)]
+    rho_Z = [random_scalar() for _ in range(m)]
     for j in range(m):
-        rho[j] = random_scalar()
-        tau[j] = random_scalar()
-        gamma = random_scalar()
-        gammas[j] = gamma
         for i in range(N):
-            G[j] += M[i]*p[i][j]
-        G[j] -= H2*gamma
-        Q[j] = comm(Scalar(0),rho[j],tau[j]) + H2*gamma
-
-    # Assemble state
-    state = State()
-    state.m = m
-    state.n = n
-    state.sigma = sigma
-    state.a = a
-    state.rA = rA
-    state.rB = rB
-    state.rC = rC
-    state.rD = rD
-    state.v = v
-    state.r = r
-    state.rho = rho
-    state.tau = tau
-    state.gammas = gammas
+            X[j] += M[i]*p[i][j]
+            Y[j] += hash_to_point(M[i])*p[i][j]
+            Z[j] += P[i]*p[i][j]
+        X[j] += rho[j]*G
+        Y[j] += rho[j]*J
+        Z[j] += rho_Z[j]*G
 
     # Partial proof
     proof = Proof()
+    proof.J = J
     proof.A = A
     proof.B = B
     proof.C = C
     proof.D = D
-    proof.G = G
-    proof.Q = Q
+    proof.X = X
+    proof.Y = Y
+    proof.Z = Z
 
-    return proof,state
+    x = hash_to_scalar(M,P,J,A,B,C,D,X,Y,Z)
 
-# Complete a partial proof
-def prove_final(proof,state):
-    x = state.x # aggregate Fiat-Shamir challenge
-
-    # Recover state
-    m = state.m
-    n = state.n
-    sigma = state.sigma
-    a = state.a
-    rA = state.rA
-    rB = state.rB
-    rC = state.rC
-    rD = state.rD
-    v = state.v
-    r = state.r
-    rho = state.rho
-    tau = state.tau
-    gammas = state.gammas
-
-    f = [[None]*n for _ in range(m)]
+    f = [[None for _ in range(n)] for _ in range(m)]
     for j in range(m):
         for i in range(1,n):
             f[j][i] = sigma[j][i]*x + a[j][i]
 
     zA = rB*x + rA
     zC = rC*x + rD
-    zV = v*x**m
     zR = r*x**m
+    zS = s*x**m
     for j in range(m):
-        zV -= rho[j]*x**j
-        zR -= tau[j]*x**j
+        zR -= rho[j]*x**j
+        zS -= rho_Z[j]*x**j
 
     # Assemble proof
     proof.f = f
     proof.zA = zA
     proof.zC = zC
-    proof.zV = zV
     proof.zR = zR
+    proof.zS = zS
 
-    return proof,gammas
+    return proof
 
 # Verify a commitment-to-zero proof
 #
 # INPUT
-#  M: list of double-blinded Pedersen commitments such that len(M) == n**m
+#  M: public key list
+#  P: value commitment list
 #  proof: proof structure
-#  n,m: dimensions such that len(M) == n**m
-#  x: aggregate Fiat-Shamir challenge
+#  m: dimension such that len(M) == len(C) == 2**m
 # RETURNS
 #  True if the proof is valid
-def verify(M,proof,n,m,x):
+def verify(M,P,proof,m):
+    n = 2
+    N = n**m
+
+    J = proof.J
     A = proof.A
     B = proof.B
     C = proof.C
     D = proof.D
-    G = proof.G
-    Q = proof.Q
+    X = proof.X
+    Y = proof.Y
+    Z = proof.Z
     f = proof.f
     zA = proof.zA
     zC = proof.zC
-    zV = proof.zV
     zR = proof.zR
+    zS = proof.zS
 
-    N = n**m
+    x = hash_to_scalar(M,P,J,A,B,C,D,X,Y,Z)
 
     for j in range(m):
         f[j][0] = x
@@ -305,7 +259,7 @@ def verify(M,proof,n,m,x):
         raise ArithmeticError('Failed A/B check!')
 
     # C/D check
-    fx = [[None]*n for _ in range(m)]
+    fx = [[None for _ in range(n)] for _ in range(m)]
     for j in range(m):
         for i in range(n):
             fx[j][i] = f[j][i]*(x-f[j][i])
@@ -313,16 +267,37 @@ def verify(M,proof,n,m,x):
         raise ArithmeticError('Failed C/D check!')
 
     # Commitment check
-    R = dumb25519.Z
+    R1 = dumb25519.Z
+    R2 = dumb25519.Z
+    S = dumb25519.Z
     for i in range(N):
         s = Scalar(1)
         decomp_i = decompose(i,n,m)
         for j in range(m):
             s *= f[j][decomp_i[j]]
-        R += M[i]*s
+        R1 += M[i]*s
+        R2 += hash_to_point(M[i])*s
+        S += P[i]*s
     for j in range(m):
-        R -= (G[j] + Q[j])*x**j
-    if not R == comm(Scalar(0),zV,zR):
+        R1 -= X[j]*x**j
+        R2 -= Y[j]*x**j
+        S -= Z[j]*x**j
+    if not R1 == zR*G or not R2 == zR*J or not S == zS*G:
         raise ArithmeticError('Failed commitment check!')
 
     return True
+
+# Basic test
+m = 3
+l = 1
+
+r = random_scalar()
+s = random_scalar()
+
+M = [random_point() for _ in range(2**m)]
+M[l] = r*G
+P = [random_point() for _ in range(2**m)]
+P[l] = s*G
+
+proof = prove(M,P,l,r,s,m)
+verify(M,P,proof,m)
